4768c9af5d0c30748be82bab7c64f88f110ca391
jeffalo <40470736+jeffalo@users.noreply.github.com>
2026-02-10 15:29:04 -0500

    use SP for post count

--- store/counts.js
+++ store/counts.js
@@ -1,55 +1,55 @@
 export const state = () => ({
-    users: []
+  users: []
 })
 
 let findUser = (state, name) => {
-    return state.users.find(user => user.name.toLowerCase() == name.toLowerCase())
+  return state.users.find(user => user.name.toLowerCase() == name.toLowerCase())
 }
 
 export const actions = {
-    async loadUser({ state, commit }, { name }) {
-        let found = findUser(state, name)
-        if (found && !found.loading) {
-            // its ready, just send it
-            return found.count
-        }
-
-        if (found && found.loading) {
-            // its loading, return the promise
-            return found.promise
-        }
-
-        // this is the first time
-
-        let promise = new Promise((resolve, reject) => {
-            fetch("https://scratchdb.lefty.one/v3/forum/user/info/" + name)
-            .then((res) => res.json())
-            .then(data => {
-                resolve(data.counts.total.count)
-                commit('setUser', { name, count: data.counts.total.count, promise, loading: false })
-            })
-          });          
-
-
-        commit('initUser', { name, promise, loading: true })
-
-        return promise
+  async loadUser({ state, commit }, { name }) {
+    let found = findUser(state, name)
+    if (found && !found.loading) {
+      // its ready, just send it
+      return found.count
     }
+
+    if (found && found.loading) {
+      // its loading, return the promise
+      return found.promise
+    }
+
+    // this is the first time
+
+    let promise = new Promise((resolve, reject) => {
+      fetch(`https://api.scratchpost.quuq.dev/users/${name}/count`)
+        .then((res) => res.json())
+        .then(data => {
+          resolve(data.count)
+          commit('setUser', { name, count: data.counts.total.count, promise, loading: false })
+        })
+    });
+
+
+    commit('initUser', { name, promise, loading: true })
+
+    return promise
+  }
 }
 
 export const mutations = {
-    setUser(state, { name, count, promise, loading }) {
-        let found = findUser(state, name)
-        if (found) {
-            found = { name, count, promise, loading }
-        } else {
-            state.users.push({ name, count, promise, loading })
-        }
-    },
-    initUser(state, { name, promise } ) {
-        let found = findUser(state, name)
-        if (!found) {
-            state.users.push({ name, count: 0, promise, loading: true })
-        }
+  setUser(state, { name, count, promise, loading }) {
+    let found = findUser(state, name)
+    if (found) {
+      found = { name, count, promise, loading }
+    } else {
+      state.users.push({ name, count, promise, loading })
     }
+  },
+  initUser(state, { name, promise }) {
+    let found = findUser(state, name)
+    if (!found) {
+      state.users.push({ name, count: 0, promise, loading: true })
+    }
+  }
 }
