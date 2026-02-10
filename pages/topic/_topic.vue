90beb51bf97084936fbd1d3d3803006d0c892d2a
jeffalo <40470736+jeffalo@users.noreply.github.com>
2026-02-10 15:44:37 -0500

    use SP for individual topic

--- pages/topic/_topic.vue
+++ pages/topic/_topic.vue
@@ -10,22 +10,22 @@
         :fullwidth="true"
       />
       <div v-if="!$fetchState.pending">
-        <div v-if="data.hits && data.hits[0]">
-          <h1 style="display: inline-block">{{ data.hits[0].topic.title }}</h1>
+        <div v-if="data && data[0]">
+          <h1 style="display: inline-block">{{ data[0].topic_name }}</h1>
           <p>
-            id: <b>{{ data.hits[0].topic.id }}</b>
+            id: <b>{{ data[0].topic_id }}</b>
           </p>
           <p>
             category:
             <b
-              ><nuxt-link :to="`/browse/${category}`">{{
-                data.hits[0].topic.category
+              ><nuxt-link :to="`/browse/${data[0].category_id}`">{{
+                data[0].category_name
               }}</nuxt-link></b
             >
           </p>
         </div>
-        <PostList
-          :posts="data.hits"
+        <PostListSP
+          :posts="data"
           :loading="$fetchState.pending"
           @loadMore="loadMore()"
           :showLoadMore="showLoadMore"
@@ -60,7 +60,7 @@ export default {
       page: 0,
       data: {},
       category: "",
-      showLoadMore: false,
+      showLoadMore: true,
       error: null,
       op: "",
     };
@@ -71,15 +71,7 @@ export default {
       this.page++;
 
       var postsRes = await fetch(
-        `https://scratchdb.lefty.one/search/indexes/forum_posts/search?hitsPerPage=50&page=${
-          this.page + 1
-        }&sort=id:asc&filter=topic.id=${this.topic}`,
-        {
-          headers: {
-            authorization:
-              "Bearer 3396f61ef5b02abf801096be5f0b0ee620de304dd92fc6045aeb99539cd0bec4",
-          },
-        }
+        `https://api.scratchpost.quuq.dev/search/posts?mode=relevance&sort=oldest&category=all&limit=100&offset=${100 * this.page}&topic=${this.topic}&detail=3&displayAuthor=true`,
       ).catch((err) => {
         this.error = {
           title: "Network Error",
@@ -87,22 +79,14 @@ export default {
         };
       });
       var postData = await postsRes.json();
-      this.data.hits.push(...postData.hits);
+      this.data.push(...postData);
 
       this.showLoadMore = true;
     },
   },
   async fetch() {
     var postsRes = await fetch(
-      `https://scratchdb.lefty.one/search/indexes/forum_posts/search?hitsPerPage=50&page=${
-        this.page + 1
-      }&sort=id:asc&filter=topic.id=${this.topic}`,
-      {
-        headers: {
-          authorization:
-            "Bearer 3396f61ef5b02abf801096be5f0b0ee620de304dd92fc6045aeb99539cd0bec4",
-        },
-      }
+      `https://api.scratchpost.quuq.dev/search/posts?mode=relevance&sort=oldest&category=all&limit=100&offset=0&topic=${this.topic}&detail=3&displayAuthor=true`,
     ).catch((err) => {
       this.error = {
         title: "Network Error",
@@ -112,8 +96,8 @@ export default {
     var postData = await postsRes.json();
 
     this.data = postData;
-    this.op = postData.hits[0].username;
-    this.category = getKey(map, postData.hits[0].topic.category); //get category id from name
+    this.op = postData[0]?.author;
+    this.category = postData[0]?.category_id 
     this.showLoadMore = true;
   },
   fetchOnServer: false,
